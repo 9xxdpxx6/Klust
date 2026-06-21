1ea6dd23a7b4e9449bc5a81b25fac19b2dc8edb9
ashiq-a-r <ashiqkallara.cet@gmail.com>
2026-06-21 15:59:35 +0530

    test tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,6 +10,14 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
+    use RefreshDatabase;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(); // runs DatabaseSeeder
+    }
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
